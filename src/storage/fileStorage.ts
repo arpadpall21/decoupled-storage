1ff6229d05beedf6d401f7c21963bab58547bd8e
Arpad <arpad.pall@zeiss.com>
2024-09-28 17:05:29 +0200

    logging fixed

--- src/storage/fileStorage.ts
+++ src/storage/fileStorage.ts
@@ -22,13 +22,14 @@ class FileStorage extends AbstractStorage {
       const fileStorage = await this.readFileStorage();
 
       if (fileStorage[post.id]) {
+        console.error(`Failed to create record, id already exist: ${post.id}`);
         return false;
       }
 
       fileStorage[post.id] = post;
       await this.writeFileStorage(fileStorage);
-      console.info('Record created: ', post);
 
+      console.info('Record created: ', post);
       return true;
     } catch {
       console.error('Failed to create record: ', post);
@@ -46,10 +47,13 @@ class FileStorage extends AbstractStorage {
           result.push(fileStorage[noteKey]);
         }
 
+        console.info('Getting all records');
         return result;
       } else if (fileStorage[id]) {
+        console.info(`Getting record with id: ${id}`);
         return fileStorage[id];
       } else {
+        console.info(`Record with id not found: ${id}`);
         return false;
       }
     } catch {
@@ -63,14 +67,17 @@ class FileStorage extends AbstractStorage {
       const fileStorage = await this.readFileStorage();
 
       if (!fileStorage[note.id]) {
+        console.info(`Failed to update record, record with id does not exist: ${note.id}`);
         return false;
       }
 
       fileStorage[note.id] = note;
       await this.writeFileStorage(fileStorage);
+
+      console.info('Record Updated: ', note);
       return true;
     } catch {
-      console.error('Failed to update note: ', note);
+      console.info(`Failed to update record with id: ${note.id}`);
       return false;
     }
   }
@@ -80,14 +87,17 @@ class FileStorage extends AbstractStorage {
       const fileStorage = await this.readFileStorage();
 
       if (!fileStorage[id]) {
+        console.info(`Failed to delete record, record with id does not exist: ${id}`);
         return false;
       }
 
       delete fileStorage[id];
       await this.writeFileStorage(fileStorage);
+
+      console.info(`Record deleted with id: ${id}`);
       return true;
     } catch {
-      console.error('Failed to delete note with id: ', id);
+      console.error(`Failed to delete record with id: ${id}`);
       return false;
     }
   }
