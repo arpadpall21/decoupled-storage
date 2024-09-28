b39c015b3650837d6ac9368a607ea71367e156d2
Arpad <arpad.pall@zeiss.com>
2024-09-28 11:28:18 +0200

    create & read ok

--- src/storage/fileStorage.ts
+++ src/storage/fileStorage.ts
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs, { promises as fsPromises } from 'node:fs';
 import AbstractStorage, { Note } from './abstract';
 import config from '../config';
 
@@ -17,18 +17,103 @@ class FileStorage extends AbstractStorage {
   }
 
   async create(post: Note): Promise<boolean> {
-    return true;
+    try {
+      const fileStorage = await this.readFileStorage();
+
+      if (fileStorage[post.id]) {
+        return false;
+      }
+
+      fileStorage[post.id] = post;
+      await this.writeFileStorage(fileStorage);
+      console.info('Record created: ', post);
+
+      return true;
+    } catch {
+      console.error('Failed to create record: ', post);
+      return false;
+    }
   }
 
   async read(id?: number): Promise<Note | Note[] | false> {
-    return false;
+    try {
+      const fileStorage = await this.readFileStorage();
+
+      if (!id) {
+        const result: Note[] = [];
+        for (const noteKey in fileStorage) {
+          result.push(fileStorage[noteKey]);
+        }
+
+        return result;
+      } else if (fileStorage[id]) {
+        return fileStorage[id];
+      } else {
+        return false;
+      }
+    } catch {
+      console.error('Failed to create record(s)');
+      return false;
+    }
+
+
+    // if (!this.conntected) {
+    //   console.error('Database read opreation failed, no database connected!');
+    //   return false;
+    // }
+
+    // if (id) {
+    //   console.info(`Getting record with id: ${id}`);
+    //   const result = await NotesModel.findOne({ _id: id });
+    //   return result ? this.mongoDocNoteToNote(result) : false;
+    // }
+
+    // console.info('Getting all records');
+    // const result = await NotesModel.find();
+    // return result.map((doc) => this.mongoDocNoteToNote(doc));
+    
+    
+    
+      // const fileStorage = await this.readFileStorage();
   }
 
   async update(note: Note): Promise<boolean> {
-    return true;
+    // if (!this.conntected) {
+    //   console.error('Database update opreation failed, no database connected!');
+    //   return false;
+    // }
+
+    // const result = await NotesModel.updateOne(
+    //   { _id: note.id },
+    //   { _id: note.id, author: note.author, content: note.content },
+    // );
+    // return result.modifiedCount === 1 ? true : false;
+    
+    
+    return false;
   }
 
   async delete(id: number): Promise<boolean> {
+    // if (!this.conntected) {
+    //   console.error('Database delete opreation failed, no database connected!');
+    //   return false;
+    // }
+
+    // const result = await NotesModel.deleteOne({ _id: id });
+    // return result.deletedCount === 1 ? true : false;
+    
+    
+    
+    return false;
+  }
+
+  private async readFileStorage(): Promise<{ [key: string]: Note }> {
+    const result = await fsPromises.readFile(filePath);
+    return JSON.parse(result.toString());
+  }
+
+  private async writeFileStorage(storage: object): Promise<boolean> {
+    await fsPromises.writeFile(filePath, JSON.stringify(storage));
     return true;
   }
 }
